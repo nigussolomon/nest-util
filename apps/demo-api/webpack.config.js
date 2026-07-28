ee42390163c559bf2ed4d59f343136b274b63d37
nigussolomon <nigussolomon444@gmail.com>
2026-07-28 19:31:04 +0300

    fix: externalize sharp in webpack to avoid native module bundling

--- apps/demo-api/webpack.config.js
+++ apps/demo-api/webpack.config.js
@@ -9,6 +9,14 @@ module.exports = {
       devtoolModuleFilenameTemplate: '[absolute-resource-path]',
     }),
   },
+  externals: {
+    sharp: 'commonjs sharp',
+    '@img/sharp-libvips-dev': 'commonjs @img/sharp-libvips-dev',
+    '@img/sharp-linux-x64': 'commonjs @img/sharp-linux-x64',
+    '@img/sharp-wasm32': 'commonjs @img/sharp-wasm32',
+    'utf-8-validate': 'commonjs utf-8-validate',
+    'bufferutil': 'commonjs bufferutil',
+  },
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
